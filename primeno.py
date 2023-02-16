0c66f5f2088e60fbb53d3a3165a71d7c8aff8698
Rahul <ramidirahulreddy@gmail.com>
2023-02-16 23:07:22 +0530

    first commit

---
+++ primeno.py
@@ -0,0 +1,13 @@
+def  isprime(n):
+    if n<=1:
+        return false
+    for i in range (2,n/2):
+        if n%i==0:
+            return false
+    return True
+
+number=int(input("enter a number :"))
+if isprime(number)==True:
+   print("prime number ")
+print("not a prime number")
+     
