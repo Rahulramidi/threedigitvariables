87c827aa80ace56ccef58e09bd128ac5a9239107
Rahul <ramidirahulreddy@gmail.com>
2023-02-16 23:32:44 +0530

    commit

---
+++ threedigitvariables/sub.py
@@ -0,0 +1,4 @@
+number=int (input("enter a number "))
+number2=int(input("enter5 another number "))
+number3=number-number2
+print("sum is ",number3)
---
+++ threedigitvariables/sum.py
@@ -0,0 +1,5 @@
+number=int (input("enter a number "))
+number2=int(input("enter5 another number "))
+number3=int(input("enter another number"))
+number4=number+number2+number3
+print("sum is ",number4)
